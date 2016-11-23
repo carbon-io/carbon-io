219b8f7c167ccc7023157fdffe5f6e8cee5dcb81
Matt <matt@objectlabs.com>
2016-11-22 16:56:03 -0800

    Now builds commit path from out of date submodules, other things

--- .git-cmds/gitcmdutils/version_utils.py
+++ .git-cmds/gitcmdutils/version_utils.py
@@ -1,7 +1,7 @@
 import semver
 import logging
 import urlparse
-import requests
+import urllib
 import json
 import os
 import sys
@@ -19,15 +19,17 @@ def add_scope(name):
 def build_uri_for_github_tag_api(package):
     return "/repos/" + GITHUB_ORG + "/" + package + "/git/refs/tags"
 
+def find_latest_matching_tag_for_package1(package_name, semver_range, current_version):
+    return semver.bump_major(current_version)
+
 def find_latest_matching_tag_for_package(package_name, semver_range):
     refs = fetch(build_uri_for_github_tag_api(package_name))
-    tags = []
-    latest_matching_tag = None
+    latest_matching_tag = "0.0.0"
     for ref in refs:
         tag = ref['ref'].replace('refs/tags/v','')
         if desugar_range_and_match(tag, semver_range):
-            latest_matching_tag = tag
-        tags.append(ref['ref'].replace('refs/tags/v',''))
+            if semver.compare(tag, latest_matching_tag) == 1:
+                latest_matching_tag = tag
     return latest_matching_tag
 
 # assumes use of 'x', following npm docs at
@@ -47,9 +49,16 @@ def desugar_range_and_match(version, semver_range):
         return False
     return True
 
-def compare_submodule_versions_to_latest(action_on_mismatch=None):
-    os.chdir('../docs/packages')
-    for (dirpath,_,_) in os.walk('.'):
+def log_info(package_name, parent, dep_semver, latest_matching_tag, current_version):
+    logging.info("module: %s" % package_name)
+    logging.info("parent: %s" % parent)
+    logging.info("dependency semver: %s" % dep_semver)
+    logging.info("latest matching tag: %s" % latest_matching_tag)
+    logging.info("current version: %s\n" % current_version)
+
+def get_out_of_date_submodules():
+    out_of_date_submodules = []
+    for (dirpath,_,_) in os.walk('..'):
         if os.path.exists(dirpath + '/package.json') and 'node_modules' not in dirpath:
             with open(dirpath + '/package.json') as package:
                 package_json = json.load(package)
@@ -60,16 +69,19 @@ def compare_submodule_versions_to_latest(action_on_mismatch=None):
                     with open(parent + '/package.json') as parent_package:
                         parent_package_json = json.load(parent_package)
                         parent_name = parent_package_json['name']
-                        latest_matching_tag = find_latest_matching_tag_for_package(name, parent_package_json['dependencies'][add_scope(name)])
-                        logging.info("module: %s" % name)
-                        logging.info("parent: %s" % strip_scope(parent_name))
-                        logging.info("dependency semver -> %s" % (parent_package_json['dependencies'][add_scope(name)]))
-                        logging.info("latest matching tag: %s" % (latest_matching_tag))
-                        logging.info("current version: %s\n" % version)
+                        parent_dependencies = parent_package_json['dependencies']
+                        latest_matching_tag = \
+                            find_latest_matching_tag_for_package1(name, parent_dependencies[add_scope(name)], version)
+                        log_info(name, 
+                                 strip_scope(parent_name), 
+                                 parent_dependencies[add_scope(name)], 
+                                 latest_matching_tag, 
+                                 version)
                         if version != latest_matching_tag:
                             logging.info("***Current version does not match latest tag***")
-                            if action_on_mismatch:
-                                action_on_mismatch(dirpath, name, version, latest_matching_tag)
+                            out_of_date_submodules.append({"path" : dirpath,
+                                                           "parent_path" : parent})
+    return out_of_date_submodules
 
 def fetch(uri, cache=None):
     parts = urlparse.urlparse(urlparse.urljoin(GITHUB_RAW, uri))
@@ -81,11 +93,23 @@ def fetch(uri, cache=None):
            os.path.exists(cache_path):
             return json.loads(open(cache_path).read())
         logging.info('fetching %s', uri)
-        req = requests.get(uri)
-        data = req.json()
+        req = urllib.urlopen(uri)
+        data = json.loads(req.read())
         if cache_path is not None:
             open(cache_path, 'w').write(json.dumps(data, indent=4,
                                                    sort_keys=True))
         return data
     else:
         raise Exception('{!s} is not a suppported scheme'.format(parts.scheme))
+
+
+
+
+
+
+
+
+
+
+
+
