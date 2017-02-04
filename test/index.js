c71492ec8f5f429408b00c527235003eb49bb135
Matt <matt@objectlabs.com>
2017-02-03 16:09:25 -0800

    Adding the actual tests would be nice

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var o = require('@carbon-io/atom').o(module).main
+var _o = require('@carbon-io/bond')._o(module)
+var testtube = require('@carbon-io/test-tube')
+
+module.exports = o({
+  _type: testtube.Test,
+  name: 'CarbonCoreTests',
+  tests: require('@carbon-io/carbon-core')._Test()
+})
