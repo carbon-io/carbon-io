9372bc7127a581260d2c6b87af296da60ab010da
Greg Banks <gbanks@gmail.com>
2017-04-10 19:49:29 -0700

    bump dependency versions and minor version + fix tests

--- test/index.js
+++ test/index.js
@@ -1,12 +1,15 @@
+var __ = require('@carbon-io/carbon-core').fibers.__(module).main
 var o = require('@carbon-io/atom').o(module).main
 var _o = require('@carbon-io/bond')._o(module)
 var testtube = require('@carbon-io/test-tube')
 
-module.exports = o({
-  _type: testtube.Test,
-  name: 'CarbonIoTests',
-  tests: [
-    require('@carbon-io/carbon-core').$Test,
-    require('@carbon-io/carbond').$Test
-  ]
+__(function() {
+  module.exports = o({
+    _type: testtube.Test,
+    name: 'CarbonIoTests',
+    tests: [
+      require('@carbon-io/carbon-core').$Test,
+      require('@carbon-io/carbond').$Test
+    ]
+  })
 })
